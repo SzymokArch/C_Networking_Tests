9ec24db55cbd602029f1908cf24ef058057f8122
SzymokArch <94990211+SzymokArch@users.noreply.github.com>
2025-04-09 15:47:10 +0200

    Added encryption :)

--- udp_client.c
+++ udp_client.c
@@ -1,4 +1,7 @@
 #include <arpa/inet.h>
+#include <sodium.h>
+#include <sodium/crypto_kx.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,40 +11,90 @@
 #define SERVER_IP "127.0.0.1"
 #define BUFFER_SIZE 1024
 
+typedef struct packet {
+	uint8_t nonce[crypto_secretbox_NONCEBYTES];
+	uint8_t encrypted[BUFFER_SIZE + crypto_secretbox_MACBYTES];
+} packet_t;
+
+void display_hex(uint8_t *str, int len)
+{
+	for (int i = 0; i < len; i++) {
+		printf("%X", str[i]);
+	}
+	printf("\n");
+}
+
 int main()
 {
-	int sockfd;
-	char buffer[BUFFER_SIZE];
-	struct sockaddr_in servaddr;
+	// Initialize sodium for encryption
+	if (sodium_init() < 0) {
+		perror("Sodium initialization failed");
+		exit(EXIT_FAILURE);
+	}
 
 	// Create UDP socket
+	int sockfd;
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("Socket creation failed");
 		exit(EXIT_FAILURE);
 	}
 
-	memset(&servaddr, 0, sizeof(servaddr));
-
 	// Set server info
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERVER_PORT);
 	servaddr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-	printf("Enter message: ");
-	fgets(buffer, BUFFER_SIZE, stdin);
+	// Set up key pairs for encryption
+	uint8_t client_pk[crypto_kx_PUBLICKEYBYTES];
+	uint8_t client_sk[crypto_kx_SECRETKEYBYTES];
 
-	// Send message to server
-	sendto(sockfd, buffer, strlen(buffer), 0,
+	uint8_t server_pk[crypto_kx_PUBLICKEYBYTES];
+
+	uint8_t client_rx[crypto_kx_SESSIONKEYBYTES];
+	uint8_t client_tx[crypto_kx_SESSIONKEYBYTES];
+
+	// Generate the client's key pair
+	crypto_kx_keypair(client_pk, client_sk);
+
+	// Send the client's public key to server
+	sendto(sockfd, client_pk, crypto_kx_PUBLICKEYBYTES, 0,
 	       (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
-	// Receive echoed message from server
+	// Recieve server's public key
 	socklen_t len = sizeof(servaddr);
-	int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
-			 (struct sockaddr *)&servaddr, &len);
-	buffer[n] = '\0';
+	recvfrom(sockfd, server_pk, crypto_kx_PUBLICKEYBYTES, 0,
+		 (struct sockaddr *)&servaddr, &len);
 
-	printf("Server: %s\n", buffer);
+	// Compute two shared keys using the server's public key and the
+	// client's secret key. client_rx will be used by the client to receive
+	// data from the server, client_tx will be used by the client to send
+	// data to the server.
+
+	if (crypto_kx_client_session_keys(client_rx, client_tx, client_pk,
+					  client_sk, server_pk) != 0) {
+		perror("Suspicious server public key, bail out");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+
+	// Initialize a message
+	char buffer[BUFFER_SIZE];
+	memset(buffer, 0, BUFFER_SIZE);
+	strcat(buffer, "Secret message from client");
+
+	// Encrypt a message inside the packet structure
+	packet_t pack = {0};
+	randombytes_buf(pack.nonce, crypto_secretbox_NONCEBYTES);
+	crypto_secretbox_easy(pack.encrypted, (uint8_t *)buffer, BUFFER_SIZE,
+			      pack.nonce, client_tx);
+
+	// Send the packet
+	sendto(sockfd, &pack, sizeof(packet_t), 0,
+	       (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
 	close(sockfd);
+
 	return 0;
 }
--- udp_server.c
+++ udp_server.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <sodium.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,13 +8,32 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
+typedef struct packet {
+	uint8_t nonce[crypto_secretbox_NONCEBYTES];
+	uint8_t encrypted[BUFFER_SIZE + crypto_secretbox_MACBYTES];
+} packet_t;
+
+void display_hex(uint8_t *str, int len)
+{
+	for (int i = 0; i < len; i++) {
+		printf("%X", str[i]);
+	}
+	printf("\n");
+}
+
 int main()
 {
-	int sockfd;
+	// Initialize sodium for encryption
+	if (sodium_init() < 0) {
+		perror("Sodium initialization failed");
+		exit(EXIT_FAILURE);
+	}
+
 	char buffer[BUFFER_SIZE];
-	struct sockaddr_in servaddr, cliaddr;
 
 	// Create UDP socket
+	int sockfd;
+	struct sockaddr_in servaddr, cliaddr;
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("Socket creation failed");
 		exit(EXIT_FAILURE);
@@ -38,19 +58,53 @@ int main()
 
 	socklen_t len = sizeof(cliaddr);
 
+	// Set up key pairs for encryption
+	uint8_t server_pk[crypto_kx_PUBLICKEYBYTES];
+	uint8_t server_sk[crypto_kx_SECRETKEYBYTES];
+
+	uint8_t client_pk[crypto_kx_PUBLICKEYBYTES];
+
+	uint8_t server_rx[crypto_kx_SESSIONKEYBYTES];
+	uint8_t server_tx[crypto_kx_SESSIONKEYBYTES];
+
+	// Generate the client's key pair
+	crypto_kx_keypair(server_pk, server_sk);
+
 	printf("UDP Server is running on port %d...\n", PORT);
 
-	while (1) {
-		int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
-				 (struct sockaddr *)&cliaddr, &len);
-		buffer[n] = '\0'; // Null-terminate received string
-		printf("Client: %s\n", buffer);
+	// Recieve client's public key
+	recvfrom(sockfd, client_pk, crypto_kx_PUBLICKEYBYTES, 0,
+		 (struct sockaddr *)&cliaddr, &len);
+	// Send server's public key
+	sendto(sockfd, server_pk, crypto_kx_PUBLICKEYBYTES, 0,
+	       (const struct sockaddr *)&cliaddr, len);
 
-		// Echo back the message
-		sendto(sockfd, buffer, strlen(buffer), 0,
-		       (const struct sockaddr *)&cliaddr, len);
+	// Compute two shared keys using the client's public key and the
+	// server's secret key. server_rx will be used by the server to receive
+	// data from the client, server_tx will be used by the server to send
+	// data to the client.
+	if (crypto_kx_server_session_keys(server_rx, server_tx, server_pk,
+					  server_sk, client_pk) != 0) {
+		perror("client public key, bail out");
+		close(sockfd);
+		exit(EXIT_FAILURE);
 	}
 
+	packet_t pack = {0};
+	// Recieve the packet sent by the client
+	recvfrom(sockfd, &pack, sizeof(packet_t), 0,
+		 (struct sockaddr *)&cliaddr, &len);
+	char decrypted[BUFFER_SIZE];
+	memset(decrypted, 0, BUFFER_SIZE);
+
+	// Decrypt the message
+	int n = crypto_secretbox_open_easy(
+	    (uint8_t *)decrypted, pack.encrypted,
+	    BUFFER_SIZE + crypto_secretbox_MACBYTES, pack.nonce, server_rx);
+
 	close(sockfd);
+
+	printf("%s\n", decrypted);
+
 	return 0;
 }
